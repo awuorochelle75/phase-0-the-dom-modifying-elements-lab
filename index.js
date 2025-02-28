4eb547051d12af893568863e4027873c373ef518
Rochelle Awuor <awuorochelle75@gmail.com>
2025-02-28 08:46:45 +0300

    adds index.html and index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+const newHeader=document.createElement("h1")
+    newHeader.id="victory"
+    newHeader.textContent="ROCHELLE is the champion"
+    document.body.append("newHeader")
